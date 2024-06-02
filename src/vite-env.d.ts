5a7d76cd2b2e03bbc75dff40e932d03db98e6128
Gytis <medelis.gytis@gmail.com>
2024-06-02 16:13:50 +0300

    trying to remove svgAcceptance for github deployment

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,16 @@
-/// <reference types="./assets/@types/svgAcceptance" />
+declare module "*.svg" {
+    import * as React from "react";
+
+    const ReactComponent: React.FunctionComponent<
+        // React.SVGProps<SVGSVGElement> & { className?: string }
+        React.ComponentProps<"svg"> & {
+            className?: string;
+            width?: string;
+            height?: string;
+        }
+    >;
+
+    export default ReactComponent;
+}
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
