ed9e80e41dc6eb39d4ad3f93c637fc762d648d73
Gytis <medelis.gytis@gmail.com>
2024-06-03 02:39:26 +0300

    temporary fix for github pages

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,19 @@
-/// <reference types="./assets/@types/svgAcceptance" />
+///// <reference types="./assets/@types/svgAcceptance" />
+
+declare module "*.svg" {
+    import * as React from "react";
+
+    const ReactComponent: React.FunctionComponent<
+        // React.SVGProps<SVGSVGElement> & { className?: string }
+        React.ComponentProps<"svg"> & {
+            className?: string;
+            width?: string;
+            height?: string;
+        }
+    >;
+
+    export default ReactComponent;
+}
+
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
